fd37876bb43056e32acbf74be7b3d5a0ec196968
Maryada Kumar Lodha <maryadakumar@SUI-C0042-X0.local>
2022-07-10 11:06:26 +0530

    Ensure dead cell is not alive

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -1,6 +1,11 @@
 module GameOfLife
   class Cell
-    def initialize
+    attr :x_coordinate, :y_coordinate, :alive
+
+    def initialize(x_coordinate, y_coordinate, alive)
+      @x_coordinate = x_coordinate
+      @y_coordinate = y_coordinate
+      @alive = alive
     end
   end
 end
--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe GameOfLife::Cell do
   context "Initialise the cell" do
     it 'should initialise the cell' do
-      expect { GameOfLife::Cell.new }.not_to raise_error
+      expect { GameOfLife::Cell.new(0, 0, TRUE) }.not_to raise_error
+    end
+
+    it 'should return false for dead cell' do
+      cell = GameOfLife::Cell.new(0, 0, FALSE)
+      expect(cell.alive).to be FALSE
     end
   end
 end
